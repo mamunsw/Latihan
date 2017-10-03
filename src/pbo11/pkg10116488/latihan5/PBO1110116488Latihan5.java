046a5e2baff5cd5c9ac823699deec4eccf2c4c5e
MSW98 <MSW98@DESKTOP-4BCNEIF>
2017-10-03 12:19:12 +0700

    Tugas 3

---
+++ src/pbo11/pkg10116488/latihan5/PBO1110116488Latihan5.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11.pkg10116488.latihan5;
+
+/**
+ *
+ * @author MSW98
+ */
+public class PBO1110116488Latihan5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Nama : Ma'mun Basyari
+        // Kelas : IF-11
+        // NIM : 10116488
+        //Deskripsi : Program ini untuk menampilkan Variable
+        int i = 3546764;
+        int iminus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iminus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+
+}
